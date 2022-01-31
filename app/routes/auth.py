1f3c639feb79d47d8da7752a2b8b556ccb9e6115
Leandro Alves <leandrofelipecunha@hotmail.com>
2022-01-31 15:46:25 -0300

    feat: add register user

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,5 +1,7 @@
 from app.decorators import Decorators as dec
 from flask import Blueprint, jsonify
+from app.models.users import user_schema
+from app.controllers.users import UsersController
 
 auth = Blueprint('auth', __name__)
 
@@ -8,3 +10,10 @@ auth = Blueprint('auth', __name__)
 @dec.required_login
 def login(token, *args, **kwargs):
     return jsonify({'token': token})
+
+
+@auth.route('/register', methods=['POST'])
+@dec.required_schema(user_schema)
+def register(body, *args, **kwargs):
+    user = UsersController.insert_user(**body)
+    return jsonify(user_schema.dump(user))
